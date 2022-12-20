20e0a7adcb74b9771b334ba647ac93a61cafffbb
若林宏哉 <103980830+wakabayashihiroya@users.noreply.github.com>
2022-12-20 05:53:28 +0000

    編集

--- pages/kyoto.vue
+++ pages/kyoto.vue
@@ -1,8 +1,14 @@
 <template>
+<nav class="navbar navbar-expand-sm navbar-dark p-4" style="background-color: #5F9EA0">
+     <a class="navbar-brand" href="#">京都を巡る</a>
+     <button class="navbar-toggler" type="button">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+</nav>
   <div class="container my-5">
-    <h1>京都紹介</h1>
+    <h1 class="border-bottom border-2 pb-2 my-3 border-success">清水寺</h1>
     <div class="container-fluid img-hidden">
-     <img src="../image/kiyomizu.jpg" class="img-fluid">
+    <img src="../image/kiyomizu.jpg" class="img-fluid">
     </div>
   </div>
   <div class="container mb-5">
