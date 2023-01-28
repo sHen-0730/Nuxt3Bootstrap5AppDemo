fc3d198ea614c2136aeeb94002ff833e9e8ed213
若林宏哉 <103980830+wakabayashihiroya@users.noreply.github.com>
2023-01-28 07:33:00 +0000

    編集

--- pages/kyoto.vue
+++ pages/kyoto.vue
@@ -29,7 +29,7 @@
     <div class="container md-5">
     <h2 class="border-bottom border-2 pb-2 my-3 border-success">{{data.section1}}</h2>
         <div class="container-fluid img-hidden">
-    <img src="~/assets/images/sanjyu.jpg" class="img-fluid mb-3">
+    <img src="~/assets/images/sajyu_c.jpg" class="img-fluid mb-3">
     </div>
           <div class="row">
             <div class="col-sm">
@@ -44,6 +44,9 @@
     <h2 class="border-bottom border-2 pb-2 my-3 border-success">見どころ</h2>
           <div class="row">
             <div class="col-sm">
+            <div class="container-fluid img-hidden">
+    <img src="~/assets/images/niou.jpg" class="img-fluid mb-3">
+    </div>
               <p class="lh-lg">清水寺は西暦７７８年に創建され、残された記録だけでも９回の焼失があるという
               歴史ある建物です。境内には「清水の舞台から飛び降りる」の語源となった本堂や、 三重塔などの国宝、重要文化財が立ち並びます。 他にも、恋愛成就などのご利益があるとされる音羽の滝、
               胎内巡りで有名な随求堂などのパワースポットもご覧いただけます。</p>
