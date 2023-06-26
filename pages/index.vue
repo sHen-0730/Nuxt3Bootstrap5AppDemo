11d1abe8e775ad738573e3fddd3dd29e6f7385fd
若林宏哉 <103980830+wakabayashihiroya@users.noreply.github.com>
2023-06-26 05:51:48 +0000

    Card内をlistに変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,33 @@
 <template>
-<nav class="navbar navbar-expand-sm navbar-dark p-4" style="background-color: #5183C3">
-     <a class="navbar-brand" href="#">クラウド円簿会計</a>
+<nav class="navbar navbar-expand-sm navbar-dark p-3" style="background-color: #5183C3">
+     <a class="navbar-brand" href="#">円簿会計</a>
      <button class="navbar-toggler" type="button">
         <span class="navbar-toggler-icon"></span>
       </button>
 </nav>
 
+<div class = "container">
+  <div class = "row">
+  <div class = "col-2">
+  <div class="card my-5" style="max-width: 18rem;">
+    <div class="card-header bg-transparent">各種入力</div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+    </div>
+  </div>
+  <div class = "col-10">
+    <div class="card my-5" style="max-width: 18rem;">
+    <div class="card-header bg-transparent">各種出力</div>
+        <div class="card-body">
+          <p class="card-text"></p>
+        </div>
+    </div>
+  </div> 
+  </div>
+  
+</div>
 
 </template>
-<script setup>
-const data = useWeb()
-</script>
