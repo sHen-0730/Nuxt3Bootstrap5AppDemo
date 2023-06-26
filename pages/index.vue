18a40b46ee9a6f54e62ea1fa4d3b7a10998fb75c
若林宏哉 <103980830+wakabayashihiroya@users.noreply.github.com>
2023-06-26 04:49:05 +0000

    編集

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="navbar navbar-expand-sm navbar-dark p-4" style="background-color: #5F9EA0">
+<nav class="navbar navbar-expand-sm navbar-dark p-4" style="background-color: #5183C3">
      <a class="navbar-brand" href="#">クラウド円簿会計</a>
      <button class="navbar-toggler" type="button">
         <span class="navbar-toggler-icon"></span>
