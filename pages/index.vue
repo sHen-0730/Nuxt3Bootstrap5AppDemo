0d05adb1102981593024f0092711a4e78be1557a
若林宏哉 <103980830+wakabayashihiroya@users.noreply.github.com>
2023-07-10 05:51:44 +0000

    編集

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,51 @@
 <template>
 <div class = "container"> 
-<nav class="navbar navbar-expand-sm navbar-dark p-3" style="background-color: #5183C3">
-     <a class="navbar-brand" href="#">円簿会計</a>
-     <button class="navbar-toggler" type="button">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-</nav>
-
+  
+    <nav class="navbar navbar-expand-sm navbar-dark p-3" style="background-color: #5183C3">
+        <a class="navbar-brand" href="#">円簿会計</a>
+        <button class="navbar-toggler" type="button">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+    </nav>
+    <div class="pt-3" aria-label="breadcrumb" style="background-color: #d3d3d3;">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <NuxtLink to="#" class="text-dark text-decoration-none">日次処理</NuxtLink>
+        </li>
+      </ol>
+    </div>
   <div class = "row">
   <div class = "col-sm-2">
   <div class="card my-5">
     <div class="card-header" style="background-color: #99cce1">各種入力</div>
-    <ul class="list-group list-group-flush ">
+    <ul class="list-group list-group-flush">
     <li class="list-group-item mx-auto my-auto">
       <button type="button" class="btn btn-light px-3 py-3 my-4">
-        <NuxtLink to="#" class="text-dark">複合仕訳入力</NuxtLink>
+        <NuxtLink to="#" class="text-dark text-decoration-none">複合仕訳入力</NuxtLink>
       </button>
     </li>
     <li class="list-group-item mx-auto my-auto">
       <button type="button" class="btn btn-light px-4 py-3 my-4">
-      <NuxtLink to="#"  class="text-dark">元帳入力</NuxtLink>
+      <NuxtLink to="#" class="text-dark text-decoration-none">元帳入力</NuxtLink>
       </button>
     </li>
     <li class="list-group-item mx-auto my-auto">
       <button type="button" class="btn btn-light px-4 py-3 my-4">
-      <NuxtLink to="#"  class="text-dark">通帳入力</NuxtLink>
+      <NuxtLink to="#" class="text-dark text-decoration-none">通帳入力</NuxtLink>
       </button>
     </li>
   </ul>
     </div>
   </div>
+
+
   <div class = "col-sm-10">
     <div class="card my-5">
     <div class="card-header" style="background-color: #a3d592">各種出力</div>
-        <div class="card-body">
-          <div class="row mx-4">
-    <div class="col">
-      1 of 3
-    </div>
-    <div class="col">
-      2 of 3
-    </div>
-    <div class="col">
-      3 of 3
-    </div>
+    <div class="card-body"></div>
   </div>
-          <p class="card-text"></p>
-        </div>
-    </div>
   </div> 
-  </div>
-  
+  </div>  
 </div>
 
 </template>
