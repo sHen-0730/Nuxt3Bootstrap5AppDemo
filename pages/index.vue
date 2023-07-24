e4dbfbfcf761403446f6c21934bdbf17de92aca1
若林宏哉 <103980830+wakabayashihiroya@users.noreply.github.com>
2023-07-24 05:52:33 +0000

    編集

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
     <ul class="list-group list-group-flush">
     <li class="list-group-item mx-auto my-auto">
       <button type="button" class="btn btn-light px-3 py-3 my-4">
-        <NuxtLink to="/journal.vue" class="text-dark text-decoration-none">複合仕訳入力</NuxtLink>
+        <NuxtLink to="jounal/index" class="text-dark text-decoration-none">複合仕訳入力</NuxtLink>
       </button>
     </li>
     <li class="list-group-item mx-auto my-auto">
@@ -42,10 +42,33 @@
   <div class = "col-sm-10">
     <div class="card my-5">
     <div class="card-header" style="background-color: #a3d592">各種出力</div>
-    <div class="card-body"></div>
+    <div class="card-body">
+      <div class = "row">
+        <ul class="list-group list-group-flush">
+          <div class = "col">
+    <li class="list-group-item mx-auto my-auto">
+      <button type="button" class="btn btn-light px-3 py-3 my-4">
+        <NuxtLink to="jounal/index" class="text-dark text-decoration-none">複合仕訳入力</NuxtLink>
+      </button>
+    </li>
+    <li class="list-group-item mx-auto my-auto">
+      <button type="button" class="btn btn-light px-4 py-3 my-4">
+      <NuxtLink to="#" class="text-dark text-decoration-none">元帳入力</NuxtLink>
+      </button>
+    </li>
+    <li class="list-group-item mx-auto my-auto">
+      <button type="button" class="btn btn-light px-4 py-3 my-4">
+      <NuxtLink to="#" class="text-dark text-decoration-none">通帳入力</NuxtLink>
+      </button>
+    </li>
+  </div>
+  <div class="col"></div>
+  </ul>
+      </div>
+    </div>
   </div>
   </div> 
-  </div>  
+ </div>   
 </div>
 
 </template>
